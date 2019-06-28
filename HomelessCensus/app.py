98d9bb449db35a879f7c9397ad386ec9688e0b38
sstoudemire21 <47166187+sstoudemire21@users.noreply.github.com>
2019-06-28 13:47:56 -0700

    Update app.py

--- HomelessCensus/app.py
+++ HomelessCensus/app.py
@@ -26,10 +26,8 @@ Base = automap_base()
 # reflect the tables
 Base.prepare(db.engine, reflect=True)
 
-# Save references to each table
-Samples_Metadata = Base.classes.sample_metadata
-Samples = Base.classes.samples
-
+# Save references to the table
+Healthcost = Base.classes.healthcost
 
 @app.route("/")
 def index():
@@ -42,62 +40,62 @@ def names():
     """Return a list of sample names."""
 
     # Use Pandas to perform the sql query
-    stmt = db.session.query(Samples).statement
+    stmt = db.session.query(Healthcost).statement
     df = pd.read_sql_query(stmt, db.session.bind)
 
     # Return a list of the column names (sample names)
-    return jsonify(list(df.columns)[2:])
+    return jsonify(list(df.columns))
 
 
 @app.route("/metadata/<sample>")
-def sample_metadata(sample):
+def healthcost(sample):
     """Return the MetaData for a given sample."""
     sel = [
-        Samples_Metadata.sample,
-        Samples_Metadata.ETHNICITY,
-        Samples_Metadata.GENDER,
-        Samples_Metadata.AGE,
-        Samples_Metadata.LOCATION,
-        Samples_Metadata.BBTYPE,
-        Samples_Metadata.WFREQ,
+        Healthcost.age,
+        Healthcost.sex,
+        Healthcost.bmi,
+        Healthcost.children,
+        Healthcost.smoker,
+        Healthcost.region,
+        Healthcost.charges,
     ]
 
-    results = db.session.query(*sel).filter(Samples_Metadata.sample == sample).all()
+    results = db.session.query(*sel).filter(Healthcost.sample == sample).all()
  
     # Create a dictionary entry for each row of metadata information
     sample_metadata = {}
     for result in results:
-        sample_metadata["sample"] = result[0]
-        sample_metadata["ETHNICITY"] = result[1]
-        sample_metadata["GENDER"] = result[2]
-        sample_metadata["AGE"] = result[3]
-        sample_metadata["LOCATION"] = result[4]
-        sample_metadata["BBTYPE"] = result[5]
-        sample_metadata["WFREQ"] = result[6]
+        sample_metadata["age"] = result[0]
+        sample_metadata["sex"] = result[1]
+        sample_metadata["bmi"] = result[2]
+        sample_metadata["children"] = result[3]
+        sample_metadata["smoker"] = result[4]
+        sample_metadata["region"] = result[5]
+        sample_metadata["charges"] = result[6]
 
     print(sample_metadata)
     return jsonify(sample_metadata)
 
 
-@app.route("/samples/<sample>")
-def samples(sample):
-    """Return `otu_ids`, `otu_labels`,and `sample_values`."""
-    stmt = db.session.query(Samples).statement
-    df = pd.read_sql_query(stmt, db.session.bind)
+# @app.route("/samples/<sample>")
+# def samples(sample):
+#     """Return `otu_ids`, `otu_labels`,and `sample_values`."""
+#     stmt = db.session.query(Healthcost).statement
+#     df = pd.read_sql_query(stmt, db.session.bind)
 
-    # Filter the data based on the sample number and
-    # only keep rows with values above 1
-    sample_data = df.loc[df[sample] > 1, ["otu_id", "otu_label", sample]]
-    # Format the data to send as json
-    data = {
-        "otu_ids": sample_data.otu_id.values.tolist(),
-        "sample_values": sample_data[sample].values.tolist(),
-        "otu_labels": sample_data.otu_label.tolist(),
-    }
-    return jsonify(data)
+#     # Filter the data based on the sample number and
+#     # only keep rows with values above 1
+#     sample_data = df.loc[df[sample] > 1, ["otu_id", "otu_label", sample]]
+#     # Format the data to send as json
+#     data = {
+#         "otu_ids": sample_data.otu_id.values.tolist(),
+#         "sample_values": sample_data[sample].values.tolist(),
+#         "otu_labels": sample_data.otu_label.tolist(),
+#     }
+#     return jsonify(data)
 
 
 if __name__ == "__main__":
     app.run()
 
-    )
+  
