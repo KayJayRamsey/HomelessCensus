6190c3e15086afd6fa225a7eeeae3e7fa536f7b4
sstoudemire21 <47166187+sstoudemire21@users.noreply.github.com>
2019-07-01 17:23:32 -0700

    Update app.py

--- HomelessCensus/app.py
+++ HomelessCensus/app.py
@@ -35,37 +35,4 @@ def index():
 
 @app.route("/plots")
 
- Plotly.d3.csv('3dbardata.csv', function (rows) {
-            function unpack(rows, key) {
-                return rows.map(function (row) { return row[key]; });
-            }
-
-            var trace1 = {
-                x: unpack(rows, 'age'), y: unpack(rows, 'bmi'), z: unpack(rows, 'charges'),
-                mode: 'markers',
-                marker: {
-                    size: 8,
-                    line: {
-                        color: 'rgba(217, 217, 217, 0.14)',
-                        width: 0.5
-                    },
-                    opacity: 0.8
-                },
-                type: 'scatter3d'
-            };
-
-
-            var data = [trace1];
-            var layout = {
-                xaxis:{title:'X AXIS TITLE'},
-                   yaxis:{title:'Y AXIS TITLE'},
-                   zaxis:{title:'Z AXIS TITLE'},
-                margin: {
-                    l: 0,
-                    r: 0,
-                    b: 0,
-                    t: 0
-                }
-            };
-            Plotly.newPlot('myDiv', data, layout);
-        });
+ 
