36eec80c24370a16850367c43ce95d331e9af27b
sstoudemire21 <47166187+sstoudemire21@users.noreply.github.com>
2019-07-01 18:03:22 -0700

    Update app.py

--- HomelessCensus/app.py
+++ HomelessCensus/app.py
@@ -34,5 +34,39 @@ def index():
     return render_template("index.html")
 
 @app.route("/plots")
-def plots():
+def sample(plots):
+
+   sel = [
+        Medical.age,
+        Medical.sex,
+        Medical.bmi,
+        Medical.children,
+        Medical.smoker,
+        Medical.region,
+        Medical.charges,
+    ]
+
+    results = db.session.query(*sel).filter(Medical).all()
  
+    # Create a dictionary entry for each row of metadata information
+    data = {}
+    for result in results:
+        Medical["age"] = result[0]
+        Medical["sex"] = result[1]
+        Medical["bmi"] = result[2]
+        Medical["children"] = result[3]
+        Medical["smoker"] = result[4]
+        Medical["region"] = result[5]
+        Medical["charges"] = result[6]
+
+    print(data)
+    return jsonify(data)
+
+@app.route("/")
+def index():
+    """Return the homepage."""
+
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
